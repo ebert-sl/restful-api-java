872096dc4e420c1eec2b208390215677e8346b77
ebert-sl <ebertis2002@gmail.com>
2024-07-13 12:24:10 -0300

    fix: CORS

--- src/main/java/java_bootcamp/restful_api/RestfulApiApplication.java
+++ src/main/java/java_bootcamp/restful_api/RestfulApiApplication.java
@@ -3,6 +3,10 @@ package java_bootcamp.restful_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 public class RestfulApiApplication {
 
